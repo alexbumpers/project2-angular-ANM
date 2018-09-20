f2bc7a40130840747a6f94a6a553c7d5c480234d
Newton Hoac <nhoac001@ucr.edu>
2018-09-20 16:29:46 -0400

    post-timeline now populates timeline through Post[] posts. Post class located in src/app/models/post.ts

--- project2-angular-anm/src/app/components/profile-page/profile-page.component.ts
+++ project2-angular-anm/src/app/components/profile-page/profile-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ElementRef } from '@angular/core';
+import { Post } from '../../models/post';
 
 @Component({
   selector: 'app-profile-page',
@@ -13,4 +14,17 @@ export class ProfilePageComponent implements OnInit {
     this.elementRef.nativeElement.ownerDocument.body.style.backgroundColor = "rgb(20, 29, 38)";
   }
 
+  posts = [
+    new Post('username',
+      'https://puu.sh/Byfzz/f8a7cf7872.png', 
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris pellentesque diam sed accumsan ultricies. Etiam tellus velit, malesuada sed gravida in, accumsan at urna.'),
+      new Post('username',
+      'https://puu.sh/ByfAV/5c82db1289.png', 
+      `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris pellentesque diam sed accumsan ultricies. 
+      Etiam tellus velit, malesuada sed gravida in, accumsan at urna. Vivamus ultrices pellentesque mattis. In euismod iaculis metus, 
+      nec gravida neque elementum in. Nam vitae libero arcu. Nullam nec interdum justo`),
+      new Post('username',
+      'https://puu.sh/ByfAV/5c82db1289.png', 
+      `cat`)
+  ];
 }
