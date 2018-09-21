87ae207dcd0c8358dca983436a92e03d0e738ee0
Newton Hoac <nhoac001@ucr.edu>
2018-09-20 22:16:49 -0400

    Some base html and css

--- project2-angular-anm/src/app/components/user-live-search/user-live-search.component.ts
+++ project2-angular-anm/src/app/components/user-live-search/user-live-search.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-user-live-search',
@@ -7,9 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserLiveSearchComponent implements OnInit {
 
-  constructor() { }
+  constructor(private elementRef: ElementRef) { }
 
   ngOnInit() {
+    this.elementRef.nativeElement.ownerDocument.body.style.backgroundColor = "rgb(33, 33, 33)";
   }
 
 }
