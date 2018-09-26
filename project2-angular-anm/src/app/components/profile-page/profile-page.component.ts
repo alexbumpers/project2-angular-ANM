c7905a270060a7211850e112553c6e3f34678de3
Newton Hoac <nhoac001@ucr.edu>
2018-09-26 01:15:42 -0400

    Created friends Service. Updated profile-page to populate through database

--- project2-angular-anm/src/app/components/profile-page/profile-page.component.ts
+++ project2-angular-anm/src/app/components/profile-page/profile-page.component.ts
@@ -1,8 +1,13 @@
+import { Friends } from './../../models/friend.model';
+import { FriendsService } from './../../services/friends.service';
+import { UsersService } from 'src/app/services/users.service';
 import { Friend } from './../../models/friend';
 import { Component, OnInit, ElementRef } from '@angular/core';
 
 import { SessionServiceService } from '../../services/session-service.service';
 import { Post } from '../../models/post';
+import { Users } from 'src/app/models/users.model';
+import { resolve } from 'q';
 
 @Component({
   selector: 'app-profile-page',
@@ -12,24 +17,23 @@ import { Post } from '../../models/post';
 export class ProfilePageComponent implements OnInit {
 
   constructor(private elementRef: ElementRef,
-    private sessionService: SessionServiceService) { }
+    private sessionService: SessionServiceService,
+    private userService: UsersService,
+    private friendService: FriendsService) { }
 
-  sessionId:string;
-
-  ngOnInit() {
-    this.elementRef.nativeElement.ownerDocument.body.style.backgroundColor = "rgb(20, 29, 38)";
-    this.sessionService.currentMessage.subscribe(message => this.sessionId = message);
-    console.log("login: " + this.sessionId);
-  }
+  sessionId: string;
+  promise: Promise<Users>;
+  currUser: Users;
+  friendsModel: Friends[];
 
   // Left column data
   // Can include more profile data is desired
   user_pic = 'https://pbs.twimg.com/profile_images/826914296093241344/jswv7reL_400x400.jpg';
-  username = 'Aaaaaaaaaaaaaaaaaaaaaveryveryverylong User Name';
+  username: string;
 
   // Center column data
   // Maybe populate w/ more as we scroll down
-  posts = [
+  posts = new Array(
     new Post('averyveryveryverylongname',
       'https://puu.sh/Byfzz/f8a7cf7872.png', 
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris pellentesque diam sed accumsan ultricies. Etiam tellus velit, malesuada sed gravida in, accumsan at urna.'),
@@ -41,17 +45,62 @@ export class ProfilePageComponent implements OnInit {
       new Post('username',
       'https://puu.sh/ByfAV/5c82db1289.png', 
       `cat`)
-  ];
+  );
 
   // Right column data
   // Should only populate up to 5-10 friends
   // Too many would make the page very long
-  friends = [
-    new Friend('averyveryveryverylongname',
-    'https://puu.sh/Byfzz/f8a7cf7872.png',
-    []),
-    new Friend('username',
-    'https://puu.sh/ByfAV/5c82db1289.png',
-    [])
-  ]
+  // friends = new Array(
+  //   new Friend('averyveryveryverylongname',
+  //   'https://puu.sh/Byfzz/f8a7cf7872.png',
+  //   []),
+  //   new Friend('username',
+  //   'https://puu.sh/ByfAV/5c82db1289.png',
+  //   [])
+  // );
+  friends: Friend[] = new Array();
+
+  ngOnInit() {
+    console.log("profile-page OnInit");
+    this.elementRef.nativeElement.ownerDocument.body.style.backgroundColor = "rgb(20, 29, 38)";
+    // Get sessionId
+    this.sessionService.currentMessage.subscribe(message => this.sessionId = message);
+    console.log("login: " + this.sessionId);
+
+    // Get friendsList
+    this.friendService.getFriendsByPerson(this.sessionId).subscribe((allFriends)=>{
+      this.friendsModel = allFriends;
+      console.log("friends: " + this.friendsModel);
+      // Retrieve data for each friend
+      this.friendsModel.forEach(element => {
+        this.promise = new Promise<Users>((resolve)=>{
+          resolve(this.userService.getUserById(''+element.isFriendsWith));
+        });
+        // Process friend data and add to friends array
+        this.promise.then((value)=>{
+          this.friends.push(
+            new Friend(value.firstName+' '+value.lastName,
+              'https://puu.sh/Byfzz/f8a7cf7872.png',
+              [])
+          );
+        })
+      });
+    });
+
+    // Get currrent User's data
+    this.promise = new Promise<Users>((resolve)=>{
+      resolve(this.userService.getUserById(this.sessionId));
+    });
+    // Populate page with current User's data
+    this.promise.then((value)=>{
+      this.currUser = value;
+      this.username = this.currUser.firstName + " " + this.currUser.lastName;
+    });
+  }
+
+  // ngDoCheck() {
+  //   console.log("profile-page DoCheck");
+  // }
+
+  
 }
